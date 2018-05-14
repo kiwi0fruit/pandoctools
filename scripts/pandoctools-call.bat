452795480f237857681c497794f521befde20767
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-05-14 17:52:35 +0700

    commit

--- scripts/pandoctools-call.bat
+++ scripts/pandoctools-call.bat
@@ -1,1 +1,15 @@
-where $PATH:file
+@echo off
+set "full_path="
+set "all_but_first="
+
+SETLOCAL
+for /F "usebackq delims=" %%a in (`^^^"%WINDIR%\System32\where.exe^^^" $PATH:%1`) do (
+    ENDLOCAL
+    set full_path=%%a
+    GOTO :break1
+)
+
+:break1
+for /F "tokens=1,*" %%a in ("%*") do set all_but_first=%%b
+
+call "%full_path%" %all_but_first%
