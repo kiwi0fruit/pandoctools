1a39d5bb36b3ae279d7ccc7e6c155f5e739f2b4b
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-05-17 00:21:38 +0700

    commit

--- pandoctools/feather/feather_helper.py
+++ pandoctools/feather/feather_helper.py
@@ -1,17 +1,17 @@
 # %%
 from pandoctools.feather import FeatherHelper
 
-f = FeatherHelper(dir='feather')
+fh = FeatherHelper(dir='feather')
 # dir=r"%USERPROFILE%\feather\document"
 # dir="feather" same as
 # dir="./feather" (default)
 
 
 # %%
-f.name('1')
+fh.name('1')
 try:
     # raise Exception
-    A, B, C = f.pull()
+    A, B, C = fh.pull()
 except:
     # calculate stuff
-    f.push(A, B, C)
+    fh.push(A, B, C)
