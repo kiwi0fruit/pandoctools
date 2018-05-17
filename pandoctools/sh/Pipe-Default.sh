0dfad37cf0b033324f9888fb79a048d721642ea8
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-05-17 17:21:54 +0700

    commit

--- pandoctools/sh/Pipe-Default.sh
+++ pandoctools/sh/Pipe-Default.sh
@@ -5,7 +5,7 @@ pre-knitty "${input_file}" | \
 pre-sugartex | \
 cat-md "${stdin_plus[@]}" | \
 pandoc "${reader_args[@]}" -t json | \
-knitty "${input_file}" "${reader_args[@]}" "${writer_args[@]}" --to-ipynb | \
+knitty "${input_file}" "${reader_args[@]}" "${writer_args[@]}" | \
 "${sugartex[@]}" | \
 pandoc-crossref "$to" | \
 pandoc -f json "${writer_args[@]}"
