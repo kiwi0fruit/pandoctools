5d246276187c3be128c0756be829cdc0f47af7c0
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-05-17 17:31:02 +0700

    commit

--- scripts/pandoctools-setpath.bat
+++ scripts/pandoctools-setpath.bat
@@ -5,8 +5,8 @@
 ::   %core_config% (<...>\site-packages\pandoctools\bat)
 ::   %user_config% (%APPDATA%\pandoc\pandoctools)
 
-if exist "%user_config%\%~2.bat" (
-    set %~1=%user_config%\%~2.bat
+if exist "%user_config%\%~2" (
+    set %~1=%user_config%\%~2
 ) else (
-    set %~1=%core_config%\%~2.bat
+    set %~1=%core_config%\%~2
 )
