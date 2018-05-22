830feadfd76c6da6c6dcf8598f77132e47b826c2
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-05-22 14:12:44 +0700

    commit

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ class PostInstallCommand(install):
         from pandoctools import pandoctools_user, pandoctools_core, pandoctools_bin
 
         # s = ShortCutter()
-        print(pandoctools_user, pandoctools_core, pandoctools_bin, flush=True)
-        # s.create_desktop_shortcut(pandoctools_user)
-        # s.create_desktop_shortcut(pandoctools_core)
-        # s.create_desktop_shortcut(pandoctools_bin)
+        print(pandoctools_user, pandoctools_core, pandoctools_bin, file='D:\\log.txt')
+        s.create_desktop_shortcut(pandoctools_user)
+        s.create_desktop_shortcut(pandoctools_core)
+        s.create_desktop_shortcut(pandoctools_bin)
         install.run(self)
 
 
