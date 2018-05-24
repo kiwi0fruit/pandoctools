06abee12e5990f789506d7f6e2a547b4936639d6
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-05-24 13:57:18 +0700

    c

--- pandoctools/feather/feather_helper.py
+++ pandoctools/feather/feather_helper.py
@@ -1,60 +1,81 @@
+"""
+Usage example in Atom/Hydrogen:
+
 # %%
-from pandoctools.feather import FeatherHelper
-
-fh = FeatherHelper(dir='feather')
-# dir=r"%USERPROFILE%\feather\document"
-# dir="feather" same as
-# dir="./feather" (default)
-
+from pandoctools.feather import FeatherHelper as fh
+# fh.setdir(r"%USERPROFILE%\feather\mydoc")
 
 # %%
 fh.name('1')
 try:
-    # raise Exception
+    # raise Exception  # <- this is a switch
     A, B, C = fh.pull()
 except:
     # calculate stuff
     fh.push(A, B, C)
-
+"""
 
 import pandas as pd
-from os.path import join, normpath, isabs, basename
+import os
+from os import path as p
 import feather
 import numpy as np
 
+_dir = ""
+_cwd = ""
 
-class FeatherHelper:
-    """
-    Helps save-to and load-from Feather format data frames.
-    """
-    def __init__(self, folder: str=None, ext: str=''):
-        self.dir = folder
-        self.ext = ('.' + ext) if (ext is not '') else ''
 
-    def save(self, arr: np.ndarray, path: str):
-        self.save_df(pd.DataFrame(arr), path)
+def setdir(dir_: str):
+    global _dir, _cwd
+    _dir = p.abspath(p.expandvars(dir_))
+    if not p.isdir(_dir):
+        os.makedirs(_dir)
+    _cwd = p.join(_dir, 'default')
 
-    def save_df(self, df: pd.DataFrame, path: str):
-        """
-        if basename(path) == path then extension is appended
-        """
-        if basename(path) == path:
-            path = path + self.ext
-        if self.dir is not None:
-            if not isabs(path):
-                path = normpath(join(self.dir, path))
-        feather.write_dataframe(df, path)
 
-    def load(self, path: str) -> np.ndarray:
-        return self.load_df(path).as_matrix()
+setdir(p.join('.', 'feather'))
 
-    def load_df(self, path: str) -> pd.DataFrame:
-        """
-        if basename(path) == path then extension is appended
-        """
-        if basename(path) == path:
-            path = path + self.ext
-        if self.dir is not None:
-            if not isabs(path):
-                path = normpath(join(self.dir, path))
-        return feather.read_dataframe(path)
+
+class FeatherHelperError(Exception):
+    pass
+
+
+def name(name_: str):
+    global _cwd
+    _cwd = p.join(_dir, name_)
+
+
+def push(*data_frames):
+    global _cwd
+    for i, df in enumerate(data_frames):
+        if isinstance(df, np.ndarray):
+            df = pd.DataFrame(df)
+            dot_ext = '.np'
+        elif isinstance(df, pd.DataFrame):
+            dot_ext = '.df'
+        else:
+            raise FeatherHelperError('Unsupported input type')
+        feather.write_dataframe(df, p.join(_cwd, str(i) + dot_ext))
+        pass
+    _cwd = p.join(_dir, 'default')
+
+
+def pull():
+    global _cwd
+    file_names = sorted([p.basename(os.fsdecode(file))  # may be p.basename() is redundant
+                         for file in os.listdir(os.fsencode(_cwd))],
+                        key=lambda filename: int(p.splitext(filename)[0]))
+    ret = []
+    for i, file_name in enumerate(file_names):
+        if i != int(p.splitext(file_name)[0]):
+            raise FeatherHelperError('Wrong file name in {}'.format(_cwd))
+        dot_ext = p.splitext(file_name)[1]
+        df = feather.read_dataframe(p.join(_cwd, file_name))
+        if dot_ext == '.np':
+            ret.append(df.as_matrix())
+        elif dot_ext == '.df':
+            ret.append(df)
+        else:
+            raise FeatherHelperError('Wrong file ext in {}'.format(_cwd))
+    _cwd = p.join(_dir, 'default')
+    return ret
