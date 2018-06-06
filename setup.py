62aebe2936d4d2914cb73881352fdc83226561c9
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-06-06 13:19:31 +0700

    moved post-install script to pandoctools-ready package

--- setup.py
+++ setup.py
@@ -1,90 +1,18 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
-from os import path as p
+
 import os
-import configparser
-import traceback
-from pandoctools.shortcut import ShortCutter
-from pandoctools.cli import pandoctools_user
 import versioneer
-import io
-import sys
 
-DEFAULTS_INI = {'profile': 'Default',
-                'out': '*.*.md',
-                'root_env': '',
-                'win_bash': r'%PROGRAMFILES%\Git\bin\bash.exe'}
-
-here = p.abspath(p.dirname(__file__))
-with open(p.join(here, 'README.rst'), encoding='utf-8') as f:
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
-
-
-class PostInstallCommand(install):
-    """
-    Post-installation for installation mode.
-    """
-    def run(self):
-        error_log = io.StringIO()
-        sc = ShortCutter(raise_errors=False, error_log=error_log)
-
-        # Set pandoctools_core:
-        if os.name == 'nt':
-            pandoctools_core = p.join(sc.site_packages, 'pandoctools', 'bat')
-            _pandoctools_core = p.join(sc.site_packages, 'pandoctools', 'sh')
-            bash_append = ' (Bash)'
-        else:
-            pandoctools_core = p.join(sc.site_packages, 'pandoctools', 'sh')
-            _pandoctools_core = pandoctools_core
-            bash_append = ''
-
-        # Create shortcuts:
-        sc.create_desktop_shortcut('pandoctools', entry_point=True)
-        ret = sc.create_menu_shortcut('pandoctools', entry_point=True)
-        pandoctools_bin = ret[1]
-
-        sc.makedirs(pandoctools_user, pandoctools_core, _pandoctools_core)
-        sc.create_desktop_shortcut(pandoctools_user, 'Pandoctools User Data')
-        sc.create_shortcut(pandoctools_core, pandoctools_user, 'Pandoctools Core Data')
-        sc.create_shortcut(_pandoctools_core, pandoctools_user, 'Pandoctools Core Data' + bash_append)
-
-        # Write INI:
-        config_file = p.join(pandoctools_user, 'Defaults.ini')
-        config = configparser.ConfigParser(interpolation=None)
-        default_sect = DEFAULTS_INI.copy()
-        if p.exists(config_file):
-            config.read(config_file)
-            try:
-                d = config.items('Default')
-                default_sect.update(dict(d))
-            except configparser.NoSectionError:
-                pass
-        default_sect['pandoctools'] = pandoctools_bin
-        if p.exists(p.expandvars(default_sect['win_bash'])):
-            pandoctools_core = _pandoctools_core
-
-        config['Default'] = default_sect
-        with io.StringIO() as file:
-            config.write(file)
-            config_str = file.getvalue()
-        try:
-            with open(config_file, 'w') as file:
-                config.write(file)
-        except:
-            print('WARNING: Failed to create ini file.\n\n' + ''.join(traceback.format_exc()), file=error_log)
-            print('File:\n{}\n\n{}'.format(config_file, config_str), file=error_log)
-
-        # Dump error log:
-        print(error_log.getvalue(), file=open(p.join(p.join(p.expanduser('~')), 'pandoctools_install_error_log.txt'), 'w', encoding="utf-8"))
-        error_log.close()
-
-        install.run(self)
 
 
 setup(
     name='pandoctools',
     version=versioneer.get_version(),
-    cmdclass={**versioneer.get_cmdclass(), **{'install': PostInstallCommand}},
+    cmdclass=versioneer.get_cmdclass(),
 
     description='Pandoc profile manager (stores any CLI filter pipelines), CLI wrapper for Panflute, other helpers.',
     long_description=long_description,
