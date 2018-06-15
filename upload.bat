f857250f8f93dd3b0bf85cda68184dc4dd0a41bd
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-06-16 00:29:26 +0700

    upload script upd

--- upload.bat
+++ upload.bat
@@ -1,6 +1,4 @@
 python setup.py sdist
-chcp 1252 > NUL
-set "PYTHONIOENCODING="
+chcp 1252 && set "PYTHONIOENCODING="
 twine upload dist/* --skip-existing
-chcp 65001 > NUL
-set "PYTHONIOENCODING=utf-8"
+chcp 65001 && set "PYTHONIOENCODING=utf-8"
