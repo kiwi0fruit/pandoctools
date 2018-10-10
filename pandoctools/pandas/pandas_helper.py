2cf1f30a8f5adca5c975970b5d446ba65114a1b2
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-10-10 14:18:09 +0700

    added ready interface to pandas helper

--- pandoctools/pandas/pandas_helper.py
+++ pandoctools/pandas/pandas_helper.py
@@ -3,16 +3,29 @@ import pandas as pd
 _knitty = False
 
 
+def ready(knitty: bool=False):
+    """
+    Set current knitty option that later can be used by ``md_table``. Initial is False.
+    """
+    global _knitty
+    _knitty = knitty
+
+
 def md_table(df: pd.DataFrame, knitty: bool or None=None, hide: bool=False) -> str:
     """
     Displays table and returns it's Markdown string.
     If knitty - do not display.
     
-    knitty: whether we run Knitty or Hydrogen
+    Parameters
+    ----------
+    knitty : whether we run Knitty or Hydrogen
         None means use previous value. Initial is False
-    hide: whether to display table at all
-    :return: Markdown table if knitty is True
-        else ""
+    hide : whether to display table via IPython
+
+    Returns
+    -------
+    Markdown table if knitty is True
+    else ""
     """
     global _knitty
 
--- update_version.bat
+++ update_version.bat
@@ -1,4 +1,4 @@
-set tagname=0.4.11
+set tagname=0.4.12
 git tag -d %tagname%
 git push --delete origin %tagname%
 git tag -a %tagname%
