802561382c685f6ad5a1ba73a193d8a009169d8c
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-10-12 13:12:31 +0700

    commit

--- upload.bat
+++ upload.bat
@@ -1,8 +1,10 @@
-::conda install -c defaults -c conda-forge twine
+:: conda install -c defaults -c conda-forge twine
 set "script_dir=%~dp0"
 cd /d "%script_dir%"
 
 python setup.py sdist
+:: sdist is a source distribution
+:: it can be changed to binary wheel
 chcp 1252 && set "PYTHONIOENCODING="
 twine upload dist/* --skip-existing
 chcp 65001 && set "PYTHONIOENCODING=utf-8"
