a0be5c242145c078f3f62f41c63c2f602fd35ae8
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2018-10-24 23:35:44 +0700

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,9 +1,4 @@
-set "script_dir=%~dp0"
-cd /d "%script_dir%"
+cd /d "%~dp0"
 
 python setup.py sdist
-:: sdist is a source distribution
-:: it can be changed to binary wheel
-chcp 1252 && set "PYTHONIOENCODING="
 twine upload dist/* --skip-existing
-chcp 65001 && set "PYTHONIOENCODING=utf-8"
