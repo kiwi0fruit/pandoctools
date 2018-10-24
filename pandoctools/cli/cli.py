7f0faed6b362a8dad59abdbd8e0b90c38c4d18b4
kiwi0fruit <peter.zagubisalo@gmail.com>
2018-10-24 19:22:10 +0700

    removing batch

--- pandoctools/cli/cli.py
+++ pandoctools/cli/cli.py
@@ -63,14 +63,14 @@ def get_profile_path(profile: str,
     """
     if p.splitext(p.basename(profile))[0] == profile:
         ext = p.basename(core_dir)[-3:]
-        profile1 = p.join(user_dir, 'Profile-{}.{}'.format(profile, ext))
-        profile2 = p.join(core_dir, 'Profile-{}.{}'.format(profile, ext))
+        profile1 = p.join(user_dir, f'Profile-{profile}.{ext}')
+        profile2 = p.join(core_dir, f'Profile-{profile}.{ext}')
         if p.isfile(profile1):
             return profile1
         elif p.isfile(profile2):
             return profile2
         else:
-            raise ValueError("Profile '{}' was not found in\n{}\nand\n{}".format(profile, user_dir, core_dir))
+            raise ValueError(f"Profile '{profile}' was not found in\n{user_dir}\nand\n{core_dir}")
     else:
         return expand_pattern(profile, input_file, cwd)
 
@@ -81,14 +81,14 @@ def read_ini(ini: str,  dir1: str,  dir2: str):
     If ini name is given (not ini path) then search in folder1, then in folder2.
     """
     if p.splitext(p.basename(ini))[0] == ini:
-        ini1 = p.join(dir1, '{}.ini'.format(ini))
-        ini2 = p.join(dir2, '{}.ini'.format(ini))
+        ini1 = p.join(dir1, f'{ini}.ini')
+        ini2 = p.join(dir2, f'{ini}.ini')
         if p.isfile(ini1):
             ini_path = ini1
         elif p.isfile(ini2):
             ini_path = ini2
         else:
-            raise ValueError("INI '{}' was not found.".format(ini))
+            raise ValueError(f"INI '{ini}' was not found.")
     else:
         ini_path = ini
 
@@ -126,67 +126,19 @@ def user_yes_no_query(message: str):
             print("Please respond with 'y' or 'n'.")
 
 
-"""
-def user_file_query():
-    import pyperclip
-    
-    yes = {'yes', 'y'}
-    no = {'no', 'n'}
-
-    def message(filepath):
-        print("Type 'y'/'yes'+Enter to use this clipboard paste as input file:\n" +
-              "--------")
-        try:
-            print(filepath)
-        except UnicodeEncodeError:
-            print(filepath.encode('utf-8'))
-        print("--------\n" +
-              "Type 'n'/'no'+Enter to exit.\n" +
-              "Type '/'+Enter to type input file manually.\n" +
-              "Type Enter to reload clipboard paste.\n\n" +
-              "Hint: on Windows Shift+right-click gives new 'Copy as Path' context menu option
-                     (pandoctools strips \"\").")
-        return filepath
-
-    message2 = "Please type input file path (or type '/'+Enter to exit):"
-
-    file_path = message(pyperclip.paste())
-    while True:
-        answer = input().lower()
-        if answer in yes:
-            return file_path.strip('"').strip()
-        elif answer in no:
-            return None
-        elif answer == '/':
-            print(message2)
-            file_path = input()
-            while (file_path is None) or (file_path == ''):
-                file_path = input()
-                if file_path == '/':
-                    return None
-                else:
-                    return file_path.strip('"').strip()
-        else:
-            file_path = message(pyperclip.paste())
-"""
-
 if os.name == 'nt':
     pandoctools_user_data = r"%APPDATA%\pandoc\pandoctools"
     pandoctools_user = p.join(os.environ["APPDATA"], "pandoc", "pandoctools")
     env_path = p.dirname(sys.executable)
     scripts_bin = p.join(env_path, "Scripts")
     pandoctools_bin = p.join(scripts_bin, "pandoctools.exe")
-    _pdt = p.normpath(p.join(p.dirname(p.abspath(__file__)), '..'))
-    pandoctools_core = p.join(_pdt, 'bat')
-    _pandoctools_core = p.join(_pdt, 'sh')
 else:
     pandoctools_user_data = "$HOME/.pandoc/pandoctools"
     pandoctools_user = p.join(os.environ["HOME"], ".pandoc", "pandoctools")
     scripts_bin = p.dirname(sys.executable)
     env_path = p.normpath(p.join(scripts_bin, ".."))
     pandoctools_bin = p.join(scripts_bin, "pandoctools")
-    pandoctools_core = p.normpath(p.join(p.dirname(p.abspath(__file__)), '..', 'sh'))
-    _pandoctools_core = pandoctools_core
+pandoctools_core = p.normpath(p.join(p.dirname(p.abspath(__file__)), '..', 'sh'))
 
 
 help_str = f"""Pandoctools is a Pandoc profile manager that stores CLI filter pipelines.
@@ -266,21 +218,16 @@ def pandoctools(input_file, profile, out, stdio, stdin, cwd, detailed_out, debug
         out = pandoctools_meta.get('out') if (out is None) else out
 
     # Read from INI config (Read profile, 'out'. Find python root env, bash on Windows):
+    config = read_ini('Defaults', pandoctools_user, pandoctools_core)
     if os.name == 'nt':
-        config = read_ini('Defaults', pandoctools_user, _pandoctools_core)
         win_bash = expandvars(config.get('Default', 'win_bash', fallback=''))
-        if p.exists(win_bash) and not p.isdir(win_bash) and (str(profile)[-4:] != '.bat'):
+        if not p.isfile(win_bash):
             # here we implicitly use the fact that
-            # ini from core sh folder (_pandoctools_core)
+            # ini from core sh folder (pandoctools_core)
             # has path to git's bash
-            global pandoctools_core
-            pandoctools_core = _pandoctools_core
-        else:
-            win_bash = None
-            config = read_ini('Defaults', pandoctools_user, pandoctools_core)
+            raise ValueError('Bash was not found by the path provided in INI file.')
     else:
         win_bash = None
-        config = read_ini('Defaults', pandoctools_user, pandoctools_core)
 
     root_env = config.get('Default', 'root_env', fallback='')
     profile = config.get('Default', 'profile', fallback=PROFILE) if (profile is None) else profile
@@ -302,12 +249,11 @@ def pandoctools(input_file, profile, out, stdio, stdin, cwd, detailed_out, debug
         with open(profile_path, 'r') as file:
             print('\nProfile code:\n\n{}\n'.format(file.read()))
         message = ("Type 'y/yes' to continue with:\n" +
-                   "    Profile: {}\n" +
-                   "    Profile path: {}\n\n" +
-                   "    Out: {}\n" +
-                   "    Out path: {}\n\n" +
-                   "Or type 'n/no' to exit. Then press Enter.").format(profile, profile_path,
-                                                                       out, output_file)
+                   f"    Profile: {profile}\n" +
+                   f"    Profile path: {profile_path}\n\n" +
+                   f"    Out: {out}\n" +
+                   f"    Out path: {output_file}\n\n" +
+                   "Or type 'n/no' to exit. Then press Enter.")
         if not user_yes_no_query(message):
             return None
 
@@ -316,23 +262,11 @@ def pandoctools(input_file, profile, out, stdio, stdin, cwd, detailed_out, debug
     if os.name == 'nt':
         env_vars['PYTHONIOENCODING'] = 'utf-8'
         env_vars['LANG'] = 'C.UTF-8'
-    if (os.name == 'nt') and (win_bash is None):
-        env_vars['import'] = r'call "{}\pandoctools-import.bat"'.format(scripts_bin)
-        env_vars['source'] = r'call "{}\path-source.bat"'.format(scripts_bin)
-        env_vars['pyprepPATH'] = r'call "{}\path-pyprep.bat"'.format(scripts_bin)
-        env_vars['r'] = r'call "{}\path-run.bat"'.format(scripts_bin)
-        env_vars['set_resolve'] = r'call "{}\pandoctools-resolve.bat"'.format(scripts_bin)
-        env_vars['resolve'] = ''
-        env_vars['setUTF8'] = 'chcp 65001 > NUL && set "PYTHONIOENCODING=utf-8"'
-    else:
-        env_vars['import'] = p.join(scripts_bin, 'pandoctools-import')
-        env_vars['source'] = p.join(scripts_bin, 'path-source')
-        env_vars['pyprepPATH'] = p.join(scripts_bin, 'path-pyprep') if (os.name != 'nt') else p.join(scripts_bin,
-                                                                                                     'path-pyprep-win')
-        env_vars['r'] = ''
-        env_vars['set_resolve'] = ''
-        env_vars['resolve'] = p.join(scripts_bin, 'pandoctools-resolve')
-        env_vars['setUTF8'] = ''
+
+    env_vars['import'] = p.join(scripts_bin, 'pandoctools-import')
+    env_vars['source'] = p.join(scripts_bin, 'path-source')
+    env_vars['pyprepPATH'] = p.join(scripts_bin, 'path-pyprep' if (os.name != 'nt') else 'path-pyprep-win')
+    env_vars['resolve'] = p.join(scripts_bin, 'pandoctools-resolve')
 
     env_vars['_user_config'] = pandoctools_user
     env_vars['_core_config'] = pandoctools_core
@@ -345,7 +279,7 @@ def pandoctools(input_file, profile, out, stdio, stdin, cwd, detailed_out, debug
     env_vars['root_env'] = root_env
 
     # convert win-paths to unix-paths if needed:
-    if (os.name == 'nt') and (win_bash is not None):
+    if os.name == 'nt':
         vars_ = ["import", "source", "scripts", "resolve", "pyprepPATH",
                  "env_path", "input_file", "output_file", "_core_config",
                  "_user_config", "root_env"]
@@ -364,9 +298,9 @@ def pandoctools(input_file, profile, out, stdio, stdin, cwd, detailed_out, debug
 
     # debug env vars:
     if debug:
-        vars_ = ['scripts', 'import', 'source', 'pyprepPATH', 'r', 'set_resolve', 'resolve',
+        vars_ = ['scripts', 'import', 'source', 'pyprepPATH', 'resolve',
                  'env_path', '_core_config', '_user_config', 'input_file', 'output_file',
-                 'root_env', 'in_ext', 'in_ext_full', 'out_ext', 'out_ext_full', 'PYTHONIOENCODING', 'LANG', 'setUTF8']
+                 'root_env', 'in_ext', 'in_ext_full', 'out_ext', 'out_ext_full', 'PYTHONIOENCODING', 'LANG']
         for var in vars_:
             print('{}: {}'.format(var, env_vars.get(var)))
         print('win_bash: ', win_bash, '\n')
@@ -376,15 +310,9 @@ def pandoctools(input_file, profile, out, stdio, stdin, cwd, detailed_out, debug
     # run pandoctools:
     bash = win_bash if (os.name == 'nt') else 'bash'
     bash_cwd = None if cwd else p.dirname(input_file)
-    if (os.name == 'nt') and (win_bash is None):
-        for key, val in env_vars.items():
-            os.environ[key] = val
-        proc = subprocess.run(profile_path, stdout=PIPE, input=doc,
-                              encoding='utf-8', cwd=bash_cwd)
-    else:
-        proc = subprocess.run([bash, profile_path], stdout=PIPE, input=doc,
-                              encoding='utf-8', cwd=bash_cwd,
-                              env={**dict(os.environ), **env_vars})
+    proc = subprocess.run([bash, profile_path], stdout=PIPE, input=doc,
+                          encoding='utf-8', cwd=bash_cwd,
+                          env={**dict(os.environ), **env_vars})
 
     if proc.stderr is not None:
         # error_stream = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
