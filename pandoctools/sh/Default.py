ce86d48a3088d94816ec3e2ea54ae2765cb0b047
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2019-02-06 16:37:55 +0700

    Update Default.py

--- pandoctools/sh/Default.py
+++ pandoctools/sh/Default.py
@@ -4,13 +4,14 @@ import sys
 def replace_mathjax():
     import re
     import os
-    
     i = []
+
     def rep(m):
         i.append(0)
         if len(i) == 1:
             return ""
         return '<script type="text/javascript" src="{}"></script>'.format(sys.argv[2])
+
     sys.stdout.write(re.sub(
         r'<script type="text/javascript"[^<]+?[Mm]ath[Jj]ax.+?</script>',
         rep, sys.stdin.read(), count=2, flags=re.DOTALL))
