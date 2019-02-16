04623775ed547e9366df37408069c045fc82fbaa
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-16 23:14:45 +0700

    commit

--- pandoctools/shared_vars/__init__.py
+++ pandoctools/shared_vars/__init__.py
@@ -1,3 +1,3 @@
 from .shared_vars import (pandoctools_user, pandoctools_user_data, pandoctools_core,
-                          PandotoolsError, env_path, search_dirs, bash_cygpath)  # noqa
+                          PandotoolsError, env_path, search_dirs, bash_cygpath, is_bin_ext_maybe)  # noqa
 from knitty.tools import load_yaml, get, strict_str, where, KnittyError  # noqa
