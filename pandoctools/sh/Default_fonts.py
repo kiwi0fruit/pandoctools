8faf54f55869806a6283e5a98f29e3a19dc09482
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-12-08 15:18:13 +0700

    commit

--- pandoctools/sh/Default_fonts.py
+++ pandoctools/sh/Default_fonts.py
@@ -8,7 +8,7 @@ fontscss = sys.argv[1]
 if p.isfile(fontscss):
     with io.open(fontscss, 'r', encoding='utf-8') as f:
         css = f.read()
-    stdout = stdin.replace('<head>', '<head>\n' + css + '\n')
+    stdout = stdin.replace('<head>', '<head><style>' + css + '</style>')
 else:
     stdout = stdin
 sys.stdout.write(stdout)
