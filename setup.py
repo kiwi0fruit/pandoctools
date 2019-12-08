c6809b5b4cb6f97c29ff623871016b2ec7c1baf5
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-12-08 21:21:07 +0700

    commit

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setup(
     python_requires='>=3.6',
     install_requires=['click', 'pyyaml', 'notebook', 'jupyter', 'libsass', 'shutilwhich-cwdpatch>=0.1.0',
                       'panflute>=1.12.4', 'knitty>=0.5.0', 'pyppdf>=0.0.11',
-                      'sugartex>=0.1.16', 'shortcutter>=0.1.19', 'numpy', 'py-open-fonts',
+                      'sugartex>=0.1.16', 'shortcutter>=0.1.19', 'numpy', 'py-open-fonts>=0.4.9',
                       'py-pandoc>=2.8.0.1', 'py-pandoc-crossref>=0.3.5.0.1', 'py-mathjax<3.0',
                       'jupyter_console>=6.0.0'],
-    # conda create -n pandoctools -c defaults -c conda-forge "python=3.7" click pyyaml notebook jupyter libsass "shutilwhich-cwdpatch>=0.1.0" "panflute>=1.12.4" "knitty>=0.5.0" "pyppdf>=0.0.11" "sugartex>=0.1.16" "shortcutter>=0.1.19" "numpy" "py-pandoc>=2.8.0.1" "py-pandoc-crossref>=0.3.5.0.1" "py-mathjax<3.0" py-open-fonts "jupyter_console>=6.0.0"
+    # conda create -n pandoctools -c defaults -c conda-forge "python=3.7" click pyyaml notebook jupyter libsass "shutilwhich-cwdpatch>=0.1.0" "panflute>=1.12.4" "knitty>=0.5.0" "pyppdf>=0.0.11" "sugartex>=0.1.16" "shortcutter>=0.1.19" "numpy" "py-pandoc>=2.8.0.1" "py-pandoc-crossref>=0.3.5.0.1" "py-mathjax<3.0" "py-open-fonts>=0.4.9" "jupyter_console>=6.0.0"
 
     include_package_data=True,
     entry_points={
