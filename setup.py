0674d6a239d8ff718ea9aaaa03c06859b58a39fd
Peter Zagubisalo <peter.zagubisalo@gmail.com>
2020-09-28 18:37:05 +0700

    2.8.0.4 for new open-fonts ver & MIT license

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ setup(
     author='Peter Zagubisalo',
     author_email='peter.zagubisalo@gmail.com',
 
-    license='MIT',
+    license='MIT License',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
